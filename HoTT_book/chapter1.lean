befe6051a523c73a156e8796cf1853ff18c1153f
NaveenMaurya749 <naveenmaurya749@gmail.com>
2025-10-10 19:48:34 +0530

    first commit

---
+++ HoTT_book/chapter1.lean
@@ -0,0 +1,98 @@
+/-
+
+# Chapter 1 : Type Theory
+
+## Product Types
+
+-/
+-- Recursor of the product type
+
+namespace MyProd
+
+-- Defining rec primitively and pr1 and pr2 from it
+def rec {α β} (γ : Type) : (α → β → γ) → α × β → γ :=
+  fun g (a, b) ↦ g a b
+
+def pr1 {α β : Type} : α × β → α := rec α (fun a _ ↦ a)
+
+def pr2 {α β : Type} : α × β → β := rec β (fun _ b ↦ b)
+
+#check rec Nat Nat.add (1,4)
+
+#print pr1
+
+-- Induction on α × β
+def ind : (γ : α × β → Type)
+  → ((x : α) → (y : β) → γ (x, y))
+  → ((z : α × β) → γ z)
+  :=
+  fun _ g z ↦ g (pr1 z) (pr2 z)
+
+-- This allows us to prove uniq
+def uniq : (z : α × β) → ((pr1 z), (pr2 z)) = z
+  := by
+  intro z
+  constructor
+
+end MyProd
+
+-- Problem 1
+-- Given functions f : α → β and g : α → γ, define their composite g ∘ f : α → γ. Show that h ∘ (g ∘ f) = (h ∘ g) ∘ f.
+
+def compose (f : α → β) (g : β → γ) : α → γ :=
+  fun x ↦ g (f x)
+
+theorem compose_assoc (f : α → β) (g : β → γ) (h : γ → δ) :
+  (compose f (compose g h)) = (compose (compose f g) h) :=
+  by
+  unfold compose
+  rfl
+
+-- Problem 2
+-- Derive the recursion principle for products rec α β
+
+namespace MyProd
+
+-- Here, let's define pr1 and pr2 primitively and rec from them
+
+def pr1' {α β : Type} : α × β → α :=
+  fun z ↦ z.1
+
+def pr2' {α β : Type} : α × β → β :=
+  fun z ↦ z.2
+
+def rec' {α β} (γ : Type) : (α → β → γ) → α × β → γ :=
+  fun g z ↦ g (pr1' z) (pr2' z)
+
+theorem rec_eq_rec' {α β} (γ : Type) (g : α → β → γ) :
+  rec γ g = rec' γ g :=
+  by
+  unfold rec rec'
+  rfl
+
+theorem rec'_def_eq {α β} (γ : Type) (g : α → β → γ) (a : α) (b : β) :
+  rec' γ g (a, b) = g a b :=
+  by
+  unfold rec'
+  rfl
+
+end MyProd
+
+namespace MySigma
+
+-- Again, projections are primitive
+
+def pr1 {α : Type} {β : α → Type} : (Σ x : α, β x) → α :=
+  fun ⟨ x, _ ⟩ ↦ x
+
+def pr2 {α : Type} {β : α → Type} : (z : Σ x : α, β x) → β (pr1 z) :=
+  fun ⟨ _, y ⟩ ↦ y
+
+def rec {α : Type} {β : α → Type} (γ : Type) :
+  ((x : α) → (y : β x) → γ) → (Σ x : α, β x) → γ :=
+  fun g z ↦ g (pr1 z) (pr2 z)
+
+def def_equaliy_valid {α : Type} {β : α → Type} (γ : Type)
+  (g : (x : α)) → (y : β )
+
+end MySigma
